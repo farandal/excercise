6d8cbce99a04eb129972793793a914db13ba9cb9
Francisco Aranda <francisco.aranda@accedo.tv>
2016-05-09 22:13:28 +0800

    updated version number, added sql updated dump

--- docs/exercise_test_dump.sql
+++ docs/exercise_test_dump.sql
@@ -1,53 +1,57 @@
--- phpMyAdmin SQL Dump
--- version 4.6.0
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.7.12, for osx10.11 (x86_64)
 --
--- Host: localhost
--- Generation Time: May 04, 2016 at 06:35 AM
--- Server version: 5.7.12
--- PHP Version: 5.5.35
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: exercise
+-- ------------------------------------------------------
+-- Server version	5.7.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `exercise`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `Brand`
 --
 
+DROP TABLE IF EXISTS `Brand`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Brand` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `description` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `description` text,
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Brand`
 --
 
-INSERT INTO `Brand` (`id`, `name`, `description`) VALUES
-(1, 'apple', 'Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services. '),
-(2, 'Microsoft', 'Microsoft is an American multinational technology company headquartered in Redmond, Washington, that develops, manufactures, licenses, supports and sells computer software, consumer electronics and personal computers and services.');
-
--- --------------------------------------------------------
+LOCK TABLES `Brand` WRITE;
+/*!40000 ALTER TABLE `Brand` DISABLE KEYS */;
+INSERT INTO `Brand` VALUES (1,'apple','Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services. ','2016-05-04 07:05:07','2016-05-04 07:05:07'),(2,'Microsoft','Microsoft is an American multinational technology company headquartered in Redmond, Washington, that develops, manufactures, licenses, supports and sells computer software, consumer electronics and personal computers and services.','2016-05-04 07:05:07','2016-05-04 07:05:07');
+/*!40000 ALTER TABLE `Brand` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Product`
 --
 
+DROP TABLE IF EXISTS `Product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Product` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idBrand` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -57,169 +61,117 @@ CREATE TABLE `Product` (
   `colourcode` varchar(6) DEFAULT 'FFFFFF',
   `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `availability` enum('In Stock','Out Of Stock','Archived') DEFAULT 'Archived'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `availability` enum('In Stock','Out Of Stock','Archived') DEFAULT 'Archived',
+  PRIMARY KEY (`id`),
+  KEY `idBrand` (`idBrand`),
+  KEY `idUser` (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Product`
 --
 
-INSERT INTO `Product` (`id`, `idBrand`, `idUser`, `name`, `description`, `price`, `colour`, `colourcode`, `createdAt`, `updatedAt`, `availability`) VALUES
-(1, 1, 1, 'Watch', 'The Apple Watch (marketed stylized as Apple logo black.svgWATCH) is a smartwatch developed by Apple Inc. It incorporates fitness tracking and health-oriented capabilities with integration with iOS and other Apple products and services.', 299, 'black', '000000', '2016-05-03 17:48:34', '2016-05-03 18:13:18', 'Archived'),
-(2, 2, 1, 'XBox', 'Xbox is a video gaming brand created and owned by Microsoft. It represents a series of video game consoles developed by Microsoft, with three consoles released in the sixth, seventh, and eighth generations respectively.', 999, 'white', 'FFFFFF', '2016-05-03 17:48:34', '2016-05-03 18:13:18', 'Archived');
-
--- --------------------------------------------------------
+LOCK TABLES `Product` WRITE;
+/*!40000 ALTER TABLE `Product` DISABLE KEYS */;
+INSERT INTO `Product` VALUES (1,1,1,'Watch','The Apple Watch (marketed stylized as Apple logo black.svgWATCH) is a smartwatch developed by Apple Inc. It incorporates fitness tracking and health-oriented capabilities with integration with iOS and other Apple products and services.',299,'black','000000','2016-05-03 17:48:34','2016-05-03 18:13:18','Archived'),(2,2,1,'XBox','Xbox is a video gaming brand created and owned by Microsoft. It represents a series of video game consoles developed by Microsoft, with three consoles released in the sixth, seventh, and eighth generations respectively.',999,'white','FFFFFF','2016-05-03 17:48:34','2016-05-03 18:13:18','Archived');
+/*!40000 ALTER TABLE `Product` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Review`
 --
 
+DROP TABLE IF EXISTS `Review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Review` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idProduct` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
   `comment` text NOT NULL,
   `rating` int(2) NOT NULL DEFAULT '0',
   `updatedAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `createdAt` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idProduct` (`idProduct`),
+  KEY `idUser` (`idUser`),
+  CONSTRAINT `idProduct` FOREIGN KEY (`idProduct`) REFERENCES `Product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Review`
 --
 
-INSERT INTO `Review` (`id`, `idProduct`, `idUser`, `comment`, `rating`, `updatedAt`, `createdAt`) VALUES
-(1, 1, 2, 'That original device was the first video game console offered by an American company after the Atari Jaguar stopped sales in 1996. It reached over 24 million units sold as of May 10, 2006.', 8, '2016-05-03 20:09:16', '2016-05-03 20:09:16'),
-(2, 1, 2, 'The successor to the Xbox 360 and Microsoft\'s most recent console, the Xbox One,[3] was revealed on May 21, 2013', 5, '2016-05-03 20:09:16', '2016-05-03 20:09:16');
-
--- --------------------------------------------------------
+LOCK TABLES `Review` WRITE;
+/*!40000 ALTER TABLE `Review` DISABLE KEYS */;
+INSERT INTO `Review` VALUES (1,1,2,'That original device was the first video game console offered by an American company after the Atari Jaguar stopped sales in 1996. It reached over 24 million units sold as of May 10, 2006.',8,'2016-05-03 20:09:16','2016-05-03 20:09:16'),(2,1,2,'The successor to the Xbox 360 and Microsoft\'s most recent console, the Xbox One,[3] was revealed on May 21, 2013',5,'2016-05-03 20:09:16','2016-05-03 20:09:16');
+/*!40000 ALTER TABLE `Review` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `User`
 --
 
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `User` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `idUserType` int(2) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `bday` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `bday` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idUserType` (`idUserType`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`idUserType`) REFERENCES `UserType` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `User`
 --
 
-INSERT INTO `User` (`id`, `idUserType`, `name`, `email`, `bday`) VALUES
-(1, 1, 'Francisco', 'farandal@gmail.com', '1985-06-10 00:00:00'),
-(2, 2, 'Adrian', 'adrian@wweco.com', '1985-06-10 00:00:00');
-
--- --------------------------------------------------------
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (1,1,'Francisco','farandal@gmail.com','1985-06-10 00:00:00'),(2,2,'Adrian','adrian@wweco.com','1985-06-10 00:00:00'),(3,1,'anonymous','anonymous@nobody.com','2016-06-10 00:00:00');
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `UserType`
 --
 
+DROP TABLE IF EXISTS `UserType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `UserType` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `simpleacl` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `simpleacl` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `UserType`
 --
 
-INSERT INTO `UserType` (`id`, `name`, `simpleacl`) VALUES
-(1, 'admin', 'create,read,update,delete'),
-(2, 'user', 'read');
+LOCK TABLES `UserType` WRITE;
+/*!40000 ALTER TABLE `UserType` DISABLE KEYS */;
+INSERT INTO `UserType` VALUES (1,'admin','create,read,update,delete'),(2,'user','read');
+/*!40000 ALTER TABLE `UserType` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Brand`
---
-ALTER TABLE `Brand`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Indexes for table `Product`
---
-ALTER TABLE `Product`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idBrand` (`idBrand`),
-  ADD KEY `idUser` (`idUser`);
-
---
--- Indexes for table `Review`
---
-ALTER TABLE `Review`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idProduct` (`idProduct`),
-  ADD KEY `idUser` (`idUser`);
-
---
--- Indexes for table `User`
---
-ALTER TABLE `User`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idUserType` (`idUserType`);
-
---
--- Indexes for table `UserType`
---
-ALTER TABLE `UserType`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Brand`
---
-ALTER TABLE `Brand`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `Product`
---
-ALTER TABLE `Product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `Review`
---
-ALTER TABLE `Review`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `User`
---
-ALTER TABLE `User`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `UserType`
---
-ALTER TABLE `UserType`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Review`
---
-ALTER TABLE `Review`
-  ADD CONSTRAINT `idProduct` FOREIGN KEY (`idProduct`) REFERENCES `Product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `idUser` FOREIGN KEY (`idUser`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `User`
---
-ALTER TABLE `User`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`idUserType`) REFERENCES `UserType` (`id`);
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-05-09 22:08:57
